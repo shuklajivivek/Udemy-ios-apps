c79c5ebb07ecd86eb30cbc80bcdb3ac922906a41
vivekshukla <vivek.shukla@wooqer.com>
2020-01-16 23:38:28 +0530

    added roll button code for dicee app

--- Dicee/Dicee/ViewController.swift
+++ Dicee/Dicee/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var randomNumberIndex1 : Int = 0;
+    var randomNumberIndex2 : Int = 0;
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+  
+    @IBAction func RollButtonPressed(_ sender: UIButton) {
+     
+        randomNumberIndex1 = Int(arc4random_uniform(6))
+        randomNumberIndex2 = Int(arc4random_uniform(6))
+    
+    }
+    
 
 }
 
