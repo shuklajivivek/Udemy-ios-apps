f71997ad2c0fe02ab11478f0eb07f22a16b1a7bc
vivekshukla <vivek.shukla@wooqer.com>
2020-01-20 13:45:46 +0530

    calculator using auto layout stackviews

---
+++ StackViewDemo/StackViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StackViewDemo
+//
+//  Created by vivek shukla on 20/01/20.
+//  Copyright © 2020 wooqer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
